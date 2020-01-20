db75f08e6e76995bd1ca4f3c57819a8f10411d6d
illya ivasyk <ilchik262@gmail.com>
2020-01-20 18:58:45 +0200

    Lesson 3

---
+++ Lesson 3/src/Circle.java
@@ -0,0 +1,23 @@
+
+public class Circle {
+
+private int r;
+private double d;
+
+	
+		public Circle() {
+		this.r=24;
+		this.d=2;
+		}
+		public  Circle(int r,int d) {
+			
+			this.r=r;
+			this.d=d;
+			}
+	
+public void result1() {
+System.out.println(3.14 * r * r);
+}
+public void result2() {
+System.out.println(d*3.14);
+}}
