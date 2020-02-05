c73ee66d6c0e3f88f18d1b15d945e33d68ef2ed1
illya ivasyk <ilchik262@gmail.com>
2020-02-05 17:33:49 +0200

    Lesson6

--- Lesson6_1/src/Lesson6_1/Aplication.java
+++ Lesson6_1/src/Lesson6_1/Aplication.java
@@ -3,7 +3,8 @@ package Lesson6_1;
 public class Aplication {
 
 	public static void main(String[] args) {
-		Worker_Month salary_month = new Worker_Month();
+		Worker_Month salary_month =
+				new Worker_Month();
 		
 		Worker_hours salary_day = new Worker_hours();
 		
---
+++ Lesson6_1/src/com/lgs/lab/interface1/Main.java
@@ -0,0 +1,9 @@
+package com.lgs.lab.interface1;
+
+import com.lgs.lab.interface2.MyCalculator;
+
+public class Main {
+
+	
+
+}
